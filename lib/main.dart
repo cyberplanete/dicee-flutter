2cf4ef63fe47bf63e59f3606427d0479d99f0ca9
Pascal Joret <pjoret@sqli.com>
2020-10-02 17:42:37 +0200

    Button -OnPressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,6 +19,32 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var lefDiceNumber = 5;
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            //flex: 2,//2 fois sa taille
+            child: FlatButton(
+              onPressed: () {
+                print('Button left got pressed');
+              },
+              child: Image(
+                image: AssetImage('images/dice$lefDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            //flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                print('Button right got pressed');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
